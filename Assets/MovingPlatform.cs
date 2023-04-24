21a0be24244ca53847064a1febd67015d34a6bbf
Shane Kelly <shaneryankelly@gmail.com>
2023-04-24 11:42:09 -0400

    moving platform
    jump broken!

---
+++ Assets/MovingPlatform.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlatform : MonoBehaviour
+{
+    private Vector3 startPosition;
+    private float moveDistance = 30;
+    public int speed = 3;
+    public bool forward = true;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPosition = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (forward)
+        {
+            transform.position += new Vector3(speed, 0f, 0f);
+        }
+        else
+        {
+
+        }
+    }
+}
